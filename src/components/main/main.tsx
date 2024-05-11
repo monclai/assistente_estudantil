f22bf8021920eb6ef07ab0b2b29d949b39a8f2e7
Matheus Monclai <matheusmonclai@outlook.com>
2024-05-11 00:02:50 -0300

    adicionado carregamento

--- src/components/main/main.tsx
+++ src/components/main/main.tsx
@@ -10,9 +10,12 @@ import Markdown from "react-markdown";
 export default function Main() {
     const [resposta, setResposta] = useState("");
     const [pergunta, setPergunta] = useState("");
+    const [carregando, setCarregando] = useState(false);
 
     async function perguntaGemini() {
-        const responseText = await novoAssistente.professor(pergunta)
+        setCarregando(true);
+        const responseText = await novoAssistente.professor(pergunta);
+        setCarregando(false);
         setResposta(responseText);
         setPergunta("");
     }
@@ -30,11 +33,8 @@ export default function Main() {
     }
 
     return (
-        // <main className="flex flex-col justify-center items-center pt-12">
         <main className="container mx-auto flex flex-col items-center justify-center pt-12 px-4">
 
-
-            {/* <div className="w-96 flex"> */}
             <div className="w-full md:w-2/3 lg:w-1/2 flex">
                 <Input type="text" value={pergunta} placeholder="Escreva sua dúvida" onChange={(e) => setPergunta(e.target.value)} onKeyDown={e => handleKeyPress(e)} />
                 <Button className="bg-blue-500 ms-2" variant="outline" onClick={perguntaGemini} >
@@ -45,11 +45,15 @@ export default function Main() {
             {resposta && (
                 <button className="font-light text-sm mt-2 justify-end" onClick={reinicarChat}>Nova conversa</button>
             )}
-            {/* <div className="flex justify-center"> */}
+
+            {carregando && (
+                <div className="font-light text-sm mt-4">
+                    Calma ai... 🤔
+                </div>
+            )}
+
             <div className="flex flex-col items-center w-full md:w-2/3 lg:w-1/2">
                 {resposta && (
-
-                    // <Card className="max-w-2xl mt-4 mb-10">
                     <Card className="w-full mt-4 mb-10">
                         <CardContent className="p-4">
                             <Markdown>
